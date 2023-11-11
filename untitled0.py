f238e7a7508dfe3fd2226fe70133c3e1935b2f1f
Jean-Etienne Molle <95053274+VBAMVP@users.noreply.github.com>
2023-11-11 18:25:51 +0100

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -4,22 +4,11 @@ import json
 import streamlit as st
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import Flow
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os
 
 # Fonctions pour les opérations Google Docs et Drive
-def authenticate_google():
-    """Authenticate with Google and return service objects for Docs and Drive."""
-    auth.authenticate_user()
-    creds, _ = default(scopes=['https://www.googleapis.com/auth/drive',
-                           'https://www.googleapis.com/auth/documents'])
-    drive_service = build('drive', 'v3', credentials=creds)
-    docs_service = build('docs', 'v1', credentials=creds)
-
 def authenticate_google(creds_json):
     """Authenticate with Google using the provided JSON credentials."""
     scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/documents']
@@ -39,39 +28,54 @@ def authenticate_google(creds_json):
                 st.session_state['creds'] = flow.credentials
 
     return st.session_state.get('creds', None)
-def create_google_doc(title):
+
+def create_google_doc(doc_title, creds):
     """Create a Google Doc with the given title."""
     try:
         docs_service = build('docs', 'v1', credentials=creds)
+        document = docs_service.documents().create(body={'title': doc_title}).execute()
+        return document.get('documentId')
     except Exception as e:
         print(e)  # Affiche l'erreur dans la console
         st.error(f"Une erreur est survenue : {e}")  # Affiche l'erreur dans l'interface Streamlit
-    document = docs_service.documents().create(body={'title': title}).execute()
-    return document.get('documentId')
+        return None
 
-def upload_image_to_drive(image_path):
+def upload_image_to_drive(image_path, creds):
     """Upload an image to Google Drive and return its ID."""
-    drive_service = build('drive', 'v3', credentials=creds)
-    file_metadata = {'name': os.path.basename(image_path), 'mimeType': 'image/png'}
-    media = MediaFileUpload(image_path, mimetype='image/png')
-    file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    return file.get('id')
+    try:
+        drive_service = build('drive', 'v3', credentials=creds)
+        file_metadata = {'name': os.path.basename(image_path), 'mimeType': 'image/png'}
+        media = MediaFileUpload(image_path, mimetype='image/png')
+        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        return file.get('id')
+    except Exception as e:
+        print(e)
+        st.error(f"Une erreur est survenue lors du téléchargement de l'image : {e}")
+        return None
 
-def insert_image_to_doc(document_id, image_id):
+def insert_image_to_doc(document_id, image_id, creds):
     """Insert an image into a Google Doc."""
-    docs_service = build('docs', 'v1', credentials=creds)
-    requests = [{'insertInlineImage': {
-                    'location': {'index': 1},
-                    'uri': f'https://drive.google.com/uc?id={image_id}',
-                    'objectSize': {'height': {'magnitude': 50, 'unit': 'PT'}, 'width': {'magnitude': 50, 'unit': 'PT'}}}}]
-    docs_service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
+    try:
+        docs_service = build('docs', 'v1', credentials=creds)
+        requests = [{'insertInlineImage': {
+                        'location': {'index': 1},
+                        'uri': f'https://drive.google.com/uc?id={image_id}',
+                        'objectSize': {'height': {'magnitude': 50, 'unit': 'PT'}, 'width': {'magnitude': 50, 'unit': 'PT'}}}}]
+        docs_service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
+    except Exception as e:
+        print(e)
+        st.error(f"Une erreur est survenue lors de l'insertion de l'image : {e}")
 
-def insert_text_to_doc(document_id, text):
+def insert_text_to_doc(document_id, text, creds):
     """Insert text into a Google Doc."""
-    docs_service = build('docs', 'v1', credentials=creds)
-    end_index = 1 # You might want to find the correct end index where to insert the text
-    requests = [{'insertText': {'location': {'index': end_index}, 'text': text}}]
-    docs_service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
+    try:
+        docs_service = build('docs', 'v1', credentials=creds)
+        end_index = 1 # You might want to find the correct end index where to insert the text
+        requests = [{'insertText': {'location': {'index': end_index}, 'text': text}}]
+        docs_service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
+    except Exception as e:
+        print(e)
+        st.error(f"Une erreur est survenue lors de l'insertion du texte : {e}")
 
 # Streamlit Interface
 st.title('Google Docs Creator')
@@ -79,42 +83,12 @@ st.title('Google Docs Creator')
 # Téléchargez le fichier client_secret.json
 uploaded_file = st.file_uploader("Upload client_secret.json", type="json")
 if uploaded_file is not None:
-    # Charger les données du fichier JSON
     creds_json = json.load(uploaded_file)
-
-    # Appel de la fonction d'authentification avec les données JSON
     creds = authenticate_google(creds_json)
     if creds:
-        # Création des services Google Docs et Drive
-        drive_service = build('drive', 'v3', credentials=creds)
-        docs_service = build('docs', 'v1', credentials=creds)
+        # La logique de l'application vient ici
 
-# Formulaire pour créer un nouveau document
-with st.form("create_doc"):
-    doc_title = st.text_input("Enter the title for the new document")
-    submitted1 = st.form_submit_button("Create Document")
-    if submitted1:
-        # Créer le document et afficher l'ID
-        document_id = create_google_doc(doc_title)
-        st.write(f"Document created with ID: {document_id}")
-
-# Télécharger et insérer l'image
-with st.form("upload_image"):
-    image_file = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg'])
-    submitted2 = st.form_submit_button("Upload and Insert Image")
-    if submitted2 and image_file:
-        # Télécharger l'image sur Google Drive et l'insérer dans le document
-        image_path = image_file.name
-        with open(image_path, "wb") as f:
-            f.write(image_file.getbuffer())
-        image_id = upload_image_to_drive(image_path)
-        insert_image_to_doc(document_id, image_id)
-        st.write("Image inserted in the document.")
-
-# Formulaire pour insérer du texte
-with st.form("insert_text"):
-    text = st.text_area("Enter text to insert into the document")
-    submitted3 = st.form_submit_button("Insert Text")
-    if submitted3:
-        insert_text_to_doc(document_id, text)
-        st.write("Text inserted in the document.")
+        # Formulaire pour créer un nouveau document
+        with st.form("create_doc"):
+            doc_title = st.text_input("Enter the title for the new document")
+            submitted1 = st.form_submit
